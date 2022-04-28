01cc363f619f627b9cacf0fd137fc4b1121d1c4a
Franta <fanda.kom@gmail.com>
2022-04-28 23:22:56 +0200

    Nejaký animace u rezervovaní: TODO když si nekdo potvrdi vyber program se zeptá jestli jsou okenka zaskrtnuta a jestli jo-> vloží jeho id prevedený na string do pole. Pole s rezervacemi + jmeno, email, cas, datum vloží do souboru FiliWriter. Pri spustení prgramu se zkontroluje co už je zabrané a ty co jsou schová aby se nedali vybrat znova.

--- Rezervacni_system/src/main/java/com/example/rezervacni_system/RezervaceController.java
+++ Rezervacni_system/src/main/java/com/example/rezervacni_system/RezervaceController.java
@@ -33,7 +33,7 @@ public class RezervaceController {
     public CheckBox stredniStulDole2;
     public CheckBox stredniStulDole3;
     public CheckBox stredniStulDole4;
-    
+
     public TextArea rezervaceSouhrn;
     public TextField jmeno;
     public TextField email;
@@ -47,8 +47,9 @@ public class RezervaceController {
         System.out.println("Hlavní menu");
 
         root = FXMLLoader.load(getClass().getResource("Hlavni-menu.fxml"));
-        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(root);;
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        ;
         stage.setScene(scene);
         stage.setMaximized(false);
         stage.setMinHeight(600);
@@ -62,7 +63,7 @@ public class RezervaceController {
 
     public void select(ActionEvent actionEvent) {
         String source2 = actionEvent.getSource().toString();
-       //vypadá to hloupě ale funguje to :)
+        //vypadá to hloupě ale funguje to :)
         String[] parts = source2.split("=");
         String part1 = parts[0];
         String part2 = parts[1];
@@ -74,8 +75,140 @@ public class RezervaceController {
         String vybrany_stul_id = part3;
         System.out.println("ID vybraného stolu je :" + vybrany_stul_id);
 
+        switch (vybrany_stul_id) {
+            case "malyStul1", "malyStul2":
+                velkyStul1.setVisible(false);
+                velkyStul2.setVisible(false);
+                velkyStul3.setVisible(false);
+                velkyStul4.setVisible(false);
+                velkyStul5.setVisible(false);
+                velkyStul6.setVisible(false);
+
+                velkyStul1.setDisable(true);
+                velkyStul2.setDisable(true);
+                velkyStul3.setDisable(true);
+                velkyStul4.setDisable(true);
+                velkyStul5.setDisable(true);
+                velkyStul6.setDisable(true);
 
 
+                stredniStulDole1.setVisible(false);
+                stredniStulDole2.setVisible(false);
+                stredniStulDole3.setVisible(false);
+                stredniStulDole4.setVisible(false);
+
+                stredniStulDole1.setDisable(true);
+                stredniStulDole2.setDisable(true);
+                stredniStulDole3.setDisable(true);
+                stredniStulDole4.setDisable(true);
+
+                stredniStulNahore1.setVisible(false);
+                stredniStulNahore2.setVisible(false);
+                stredniStulNahore3.setVisible(false);
+                stredniStulNahore4.setVisible(false);
+
+                stredniStulNahore1.setDisable(true);
+                stredniStulNahore2.setDisable(true);
+                stredniStulNahore3.setDisable(true);
+                stredniStulNahore4.setDisable(true);
+
+
+                break;
+
+            case "velkyStul1", "velkyStul2", "velkyStul3", "velkyStul4", "velkyStul5", "velkyStul6":
+                malyStul1.setVisible(false);
+                malyStul2.setVisible(false);
+
+                malyStul1.setDisable(false);
+                malyStul2.setDisable(false);
+
+                stredniStulDole1.setVisible(false);
+                stredniStulDole2.setVisible(false);
+                stredniStulDole3.setVisible(false);
+                stredniStulDole4.setVisible(false);
+
+                stredniStulDole1.setDisable(true);
+                stredniStulDole2.setDisable(true);
+                stredniStulDole3.setDisable(true);
+                stredniStulDole4.setDisable(true);
+
+                stredniStulNahore1.setVisible(false);
+                stredniStulNahore2.setVisible(false);
+                stredniStulNahore3.setVisible(false);
+                stredniStulNahore4.setVisible(false);
+
+                stredniStulNahore1.setDisable(true);
+                stredniStulNahore2.setDisable(true);
+                stredniStulNahore3.setDisable(true);
+                stredniStulNahore4.setDisable(true);
+                break;
+
+            case "stredniStulDole1", "stredniStulDole2", "stredniStulDole3", "strednStulDole4":
+                malyStul1.setVisible(false);
+                malyStul2.setVisible(false);
+
+                malyStul1.setDisable(false);
+                malyStul2.setDisable(false);
+
+                stredniStulNahore1.setVisible(false);
+                stredniStulNahore2.setVisible(false);
+                stredniStulNahore3.setVisible(false);
+                stredniStulNahore4.setVisible(false);
+
+                stredniStulNahore1.setDisable(true);
+                stredniStulNahore2.setDisable(true);
+                stredniStulNahore3.setDisable(true);
+                stredniStulNahore4.setDisable(true);
+
+                velkyStul1.setVisible(false);
+                velkyStul2.setVisible(false);
+                velkyStul3.setVisible(false);
+                velkyStul4.setVisible(false);
+                velkyStul5.setVisible(false);
+                velkyStul6.setVisible(false);
+
+                velkyStul1.setDisable(true);
+                velkyStul2.setDisable(true);
+                velkyStul3.setDisable(true);
+                velkyStul4.setDisable(true);
+                velkyStul5.setDisable(true);
+                velkyStul6.setDisable(true);
+                break;
+
+            case "stredniStulNahore1", "stredniStulNahore2", "stredniStulNahore3", "strednStulnNahore4":{
+
+                malyStul1.setVisible(false);
+                malyStul2.setVisible(false);
+
+                malyStul1.setDisable(false);
+                malyStul2.setDisable(false);
+
+                stredniStulDole1.setVisible(false);
+                stredniStulDole2.setVisible(false);
+                stredniStulDole3.setVisible(false);
+                stredniStulDole4.setVisible(false);
+
+                stredniStulDole1.setDisable(true);
+                stredniStulDole2.setDisable(true);
+                stredniStulDole3.setDisable(true);
+                stredniStulDole4.setDisable(true);
+
+
+                velkyStul1.setVisible(false);
+                velkyStul2.setVisible(false);
+                velkyStul3.setVisible(false);
+                velkyStul4.setVisible(false);
+                velkyStul5.setVisible(false);
+                velkyStul6.setVisible(false);
+
+                velkyStul1.setDisable(true);
+                velkyStul2.setDisable(true);
+                velkyStul3.setDisable(true);
+                velkyStul4.setDisable(true);
+                velkyStul5.setDisable(true);
+                velkyStul6.setDisable(true);
+                break;
+            }
+        }
     }
-    }
-
+}
